31c781d90604cb3b00a7dd2605a041603a41bdc6
cristianmetz <c.metz@outlook.com>
2021-10-04 21:25:42 -0400

    Added properties to kitchen.js

--- Practice/03_12/Kitchen.js
+++ Practice/03_12/Kitchen.js
@@ -1,8 +1,8 @@
 // Class kitchen, to be reused
 
 class Kitchen {
-//creating the class and defining it's parameters
-    constructor (
+  //creating the class and defining it's parameters
+  constructor(
     size,
     color,
     cabinets,
@@ -11,10 +11,27 @@ class Kitchen {
     forks,
     plates,
     dishWasher,
+    lidOpen,
     cups,
     fryingPan
-)
-//define the class properties
+  ) {
+    //define the class properties
+    this.size = size;
+    this.color = color;
+    this.cabinets = cabinets;
+    this.drawers = drawers;
+    this.knifes = knifes;
+    this.forks = forks;
+    this.plates = plates;
+    this.dishWasher = dishWasher;
+    this.lidOpen = lidOpen;
+    this.cups = cups;
+    this.fryingPan = fryingPan;
+  }
+
+  togglelid(lidStatus) {
+    this.lidOpen = lidStatus;
+  }
 }
 
 export default Kitchen;
