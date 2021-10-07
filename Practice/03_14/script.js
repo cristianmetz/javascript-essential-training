7f36883ed610e6e5787e62b56d4bfbedf04946c3
Cristian Metz <c.metz@outlook.com>
2021-10-07 14:48:22 -0400

    Added four new book objects

--- Practice/03_14/script.js
+++ Practice/03_14/script.js
@@ -5,6 +5,8 @@ Add properties and methods.
 Add at least five book objects
 */
 
+//Class Book roperties name,author,bookNum,genre,frontCondition,backCondition,inUse
+
 import Book from "./Book.js";
 
 const fictionBook = new Book(
@@ -27,4 +29,52 @@ fictionBook.condition.front = "damaged";
 console.log(
   "The Fiction Book object's front cover is:",
   fictionBook.condition.front
+);
+
+const narutoBook = new Book (
+  "Ninja Naruto",
+  "Masashi Kishimoto",
+  243,
+  "Fiction",
+  "Used",
+  "Used",
+  true
+);
+
+console.log("The Ninja Naruto book:", narutoBook);
+
+const billBurr = new Book (
+  "Bill Burr Biography",
+  "Bill Burr",
+  1,
+  "Autobigraphy",
+  "New",
+  "New",
+  false
+);
+
+console.log("The Bill Burr Autobiography:", billBurr);
+
+billBurr.toggleConditon(true)
+
+const onePiece = new Book(
+  "One Peace",
+  "Eiichiro Oda",
+  12,
+  "Fiction",
+  "New",
+  "Like New",
+  false
+);
+
+console.log("This is the object One Piece:", onePiece);
+
+const itWorld = new Book(
+  "IT World",
+  "Brian Paone",
+  1,
+  "Horror",
+  "Used",
+  "Used",
+  false
 );
