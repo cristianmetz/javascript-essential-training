55ee71a47b206c77c7f2601102bd60c882b3d5be
cristianmetz <c.metz@outlook.com>
2021-10-09 19:05:54 -0400

    completed practice exercise 05_04

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -9,10 +9,25 @@
  * Use the console (script.js file) to find those target elements
  * Target specific elements nested inside other elements
  * Try advanced CSS queries to get specific targets
- * Experinent with querySelector and querySelectorAll
+ * Experinent with both querySelector and querySelectorAll
  *
  * Advanced
  * Try populating a variable with an element higher up in the DOM
  * tree hyerarchy and then use a querySelector on that variable
  * to find one of its children
  */
+
+//Makes the 5th button background red
+document.querySelectorAll("button");
+var x = document.querySelectorAll("button");
+x[5].style.backgroundColor = "red";
+
+//Makes the background color if the article#pack02 green
+document.querySelector("article#pack02");
+var y = document.querySelector("article#pack02");
+y.style.backgroundColor = "Green";
+
+//Makes the lid status of backpack02 orange
+document.querySelectorAll("li.feature.backpack__lid");
+var z = document.querySelectorAll("li.feature.backpack__lid");
+z[1].style.backgroundColor = "Orange";
