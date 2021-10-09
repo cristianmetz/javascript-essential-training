fab6c34f5378f1b53f4299b2b200254e0a86f25e
cristianmetz <c.metz@outlook.com>
2021-10-09 10:09:59 -0400

    Added tasks to 05_04

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,17 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+
+/**
+ * Tasks:
+ * Identify specific elements you want to target
+ * Use the console (script.js file) to find those target elements
+ * Target specific elements nested inside other elements
+ * Try advanced CSS queries to get specific targets
+ * Experinent with querySelector and querySelectorAll
+ *
+ * Advanced
+ * Try populating a variable with an element higher up in the DOM
+ * tree hyerarchy and then use a querySelector on that variable
+ * to find one of its children
+ */
