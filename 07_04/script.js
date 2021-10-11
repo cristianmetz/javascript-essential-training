cfd885d3783a562ee1d958179c371f2c309bdcf9
Cristian Metz <c.metz@outlook.com>
2021-10-11 14:21:41 -0400

    completed exercise 07_04

--- 07_04/script.js
+++ 07_04/script.js
@@ -1,11 +1,30 @@
 /**
  * Challenge: Build and modify an array
- * - Build an array with 8 items
- * - Remove the last item
- * - Add the last item as the first item on the array 
- * - Sort the items by alphabetical order
- * - Use the find() method to find a specific item in the array
- * - Remove the item you found using the find method from the array.
+ * - 1. Build an array with 8 items
+ * - 2. Remove the last item
+ * - 3. Add the last item as the first item on the array 
+ * - 4. Sort the items by alphabetical order
+ * - 5. Use the find() method to find a specific item in the array
+ * - 6. Remove the item you found using the find method from the array.
  */
+
+
+//Task 1
+const workDesk = ["computer", "water bottle", "docking station", "phone", "stappler", "alcohol wipes", "laptops", "notpad"];
+console.log("This is the work desk array:", workDesk);
+//Task 2
+console.log(workDesk.pop());
+//Task 3
+console.log(workDesk.unshift("notepad"));
+//Task 4
+console.log(workDesk.sort());
+//Task 5
+const found = workDesk.find(item => item === "phone");
+console.log(found);
+//Task 6
+let remove = "phone"
+workDesk.splice(workDesk.indexOf(remove), 1)
+console.log(`Array with "${remove}" removed:`, workDesk)
+
 
 
