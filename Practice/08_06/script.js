9ec28bce87d277f2a64753864abd0917fab1909f
cristianmetz <c.metz@outlook.com>
2021-10-13 19:21:02 -0400

    completed practice 08_06

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,44 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+/**
+ * 1. Create a basic function declaration
+ * 2. Make a change to an element in the DOM (querySelector, adding a class, and so on)
+ * 3. Call the function declaration
+ * 4. Create a basic function expression
+ *
+ * Additional practice
+ * A. Find an element
+ * B. Make a change to it
+ * C. Call the function
+ * D. Confirm that it works
+ * E. Reapeat with an arrow function
+ */
+
+//Task 1
+function letsMultiply(a, b) {
+  let c = a * b;
+  return c;
+}
+
+//Task 2
+document.querySelector("main").classList.add("new-class");
+
+//Task 3
+console.log("Let's multiply result:", letsMultiply(5, 5));
+
+//Task 4
+
+const letsAdd = function (a = 5, b = 5) {
+  let c = a + b;
+  return c;
+};
+console.log("Let's add function results:", letsAdd());
+
+const letsSubtract = () => {
+  let a = 100,
+    b = 50;
+  return a - b;
+};
+console.log(letsSubtract());
