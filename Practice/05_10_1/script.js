09c70fbfbe9c016a873d2f61298a92c46e4e19b4
Cristian Metz <c.metz@outlook.com>
2021-10-28 15:55:10 -0400

    Exercise practice

--- Practice/05_10_1/script.js
+++ Practice/05_10_1/script.js
@@ -64,3 +64,34 @@ main.append(newArticle);
 /**
  * Add navigation section to the DOM
  */
+
+/**
+ * Challenge: Add a new element
+ * - In JavaScript, create a new element to hold a navigation menu
+ * - Add an unordered list and a series of no less than five links to the list
+ * - Use single words like “home”, “about”, etc for the list items and set the src attribute to # for simplicity
+ * - Add the new navigation element to the DOM directly after the header
+ * - Write basic CSS and add classes as necessary to create a horizontal layout for the menu.
+ * - A tip: Use either display flex or display grid to create the horizontal menu.
+ */
+
+
+
+
+
+ const list = `
+ <li><a href="#"> Home </a> </li>
+ <li><a href="#"> About </a> </li>
+ <li><a href="#"> Projects </a> </li>
+ <li><a href="#"> Contact </a> </li>
+ <li><a href="#"> ETC </a> </li>
+ `;
+
+
+const mainNav = document.createElement("nav");
+mainNav.classList.add("navigation");
+const ulList = document.createElement("ul");
+ulList.innerHTML = list;
+mainNav.append(ulList);
+
+document.querySelector(".siteheader").append(mainNav);
