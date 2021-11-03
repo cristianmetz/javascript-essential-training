ef454844712230b2386d6fd0b3c78b2bbeb75a96
cristianmetz <c.metz@outlook.com>
2021-11-03 14:53:39 -0400

    practicing chapter 8

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -46,3 +46,38 @@ const letsSubtract = () => {
   return a - b;
 };
 console.log(letsSubtract());
+
+/**
+ * 1. Create a basic function declaration
+ * 2. Make a change to an element in the DOM (querySelector, adding a class, and so on)
+ * 3. Call the function declaration
+ * 4. Create a basic function expression
+ *
+ * Additional practice
+ * A. Find an element
+ * B. Make a change to it
+ * C. Call the function
+ * D. Confirm that it works
+ * E. Reapeat with an arrow function
+ */
+
+//Task 1
+function adding(h = 15, e = 20) {
+  let g = h + e;
+  return g;
+}
+
+//Task 2
+const firstArticle = document.createElement("article");
+firstArticle.classList.add("id", "first-article");
+document.body.append(firstArticle);
+
+//Task 3
+console.log(adding(10, 10));
+
+//Task 4
+const functionDeclaration = function (o, p) {
+  let k = o * p;
+  return k;
+};
+console.log(functionDeclaration(6, 6));
